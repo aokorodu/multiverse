c61f1b479e80386413e52c3180778a6c762e0b6e
aokorodu <aokorodu@gmail.com>
2024-07-02 08:42:26 -0400

    intro stub

--- the-multiverse/src/App.jsx
+++ the-multiverse/src/App.jsx
@@ -2,15 +2,26 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import TheScene from "./TheScene";
 import Intro from "./Intro";
+import Button from "@mui/material/Button";
 
 function App() {
   useEffect(() => {}, []);
 
+  const [page, setPage] = useState(0);
+  const changePage = () => {
+    setPage(page + 1);
+  };
+
   return (
     <>
       <div id="holder">
-        <Intro />
-        {/* <TheScene /> */}
+        {page == 0 && <Intro />}
+        {page == 1 && <TheScene />}
+        {page == 0 && (
+          <Button variant="text" onClick={changePage}>
+            BEGIN
+          </Button>
+        )}
       </div>
     </>
   );
